f5c624df1d43c6be90e8cec8da12a2731eafbe94
Horace Grindelwald <149716547+horacegrindelwald@users.noreply.github.com>
2023-12-09 15:27:57 -0500

    Add Hogwarts Express ticket generator

---
+++ games/ticket/script.js
@@ -0,0 +1,19 @@
+var today = new Date();
+const dd = String(today.getDate());
+const mm = today.toLocaleString('default', { month: 'long' }).substring(0, 3);
+const yyyy = today.getFullYear();
+const choice = 'ABCDEF'
+
+today = dd + ' ' + mm + ' ' + yyyy;
+
+document.getElementById('date').innerHTML = today;
+document.getElementById('date2').innerHTML = today;
+
+document.getElementById('car').innerHTML = Math.round(Math.random() * 14 + 1, 0);
+
+document.getElementById('seat').innerHTML = Math.round(Math.random() * 5 + 1, 0) + choice[Math.floor(Math.random() * choice.length)];
+
+const randBarcode = Math.round(Math.random() * 88888888) + 11111111;
+
+document.getElementById('bc1').innerHTML = randBarcode;
+document.getElementById('bc2').innerHTML = randBarcode;
