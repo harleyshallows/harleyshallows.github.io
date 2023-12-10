4e688c1fc91c8e7525571f8f2988e680bbf0a0e9
Horace Grindelwald <149716547+horacegrindelwald@users.noreply.github.com>
2023-12-10 00:43:25 -0500

    Include my initials in the barcode number

--- games/ticket/script.js
+++ games/ticket/script.js
@@ -13,7 +13,7 @@ document.getElementById('car').innerHTML = Math.round(Math.random() * 14 + 1, 0)
 
 document.getElementById('seat').innerHTML = Math.round(Math.random() * 5 + 1, 0) + choice[Math.floor(Math.random() * choice.length)];
 
-const randBarcode = Math.round(Math.random() * 88888888) + 11111111;
+const randBarcode = 'HG' + (Math.round(Math.random() * 888888) + 111111);
 
 document.getElementById('bc1').innerHTML = randBarcode;
 document.getElementById('bc2').innerHTML = randBarcode;
